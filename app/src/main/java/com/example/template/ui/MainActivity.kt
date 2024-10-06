60219653658fe6700295ebea3dc9485f98d2ee91
William Porto <powilliam19@gmail.com>
2024-10-06 11:37:09 -0300

    edge to edge layout

--- app/src/main/java/com/example/template/ui/MainActivity.kt
+++ app/src/main/java/com/example/template/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.template.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.ui.Modifier
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MaterialTheme {
                 Scaffold { paddingValues ->
