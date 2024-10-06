4f8b3f4e27fbcf21a4fbc34ac30a5f12f0d79bba
William Porto <powilliam19@gmail.com>
2024-10-06 12:09:52 -0300

    centering it

--- app/src/main/java/com/example/template/ui/MainActivity.kt
+++ app/src/main/java/com/example/template/ui/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.template.ui.theming.MainTheme
 
@@ -17,7 +20,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainTheme({ this }) {
                 Scaffold { paddingValues ->
-                    Text(modifier = Modifier.padding(paddingValues), text = "Hello World")
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = "Hello World"
+                        )
+                    }
                 }
             }
         }
