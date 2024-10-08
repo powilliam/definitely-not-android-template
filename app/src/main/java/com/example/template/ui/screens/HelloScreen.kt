469306e72077b0fee5ba7cd464675c194f89ca06
William Porto <powilliam19@gmail.com>
2024-10-08 08:53:18 -0300

    previews

--- app/src/main/java/com/example/template/ui/screens/HelloScreen.kt
+++ app/src/main/java/com/example/template/ui/screens/HelloScreen.kt
@@ -1,5 +1,6 @@
 package com.example.template.ui.screens
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.template.ui.theming.MainTheme
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -26,5 +30,15 @@ fun HelloScreen() {
                 text = "Hello World"
             )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun HelloScreenPreview() {
+    val context = LocalContext.current
+
+    MainTheme({ context }) {
+        HelloScreen()
     }
 }
